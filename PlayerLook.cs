44bf6d95ecc72b13596769f1e338858cbfbcf6ac
Little Game Studio <97639146+LittleGameSudio@users.noreply.github.com>
2022-06-27 10:12:58 +0200

    Add files via upload

---
+++ PlayerLook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] PlayerWallrun wallRun;
+    [SerializeField] PlayerMovement movement;
+
+    [SerializeField] private float sensX = 100f;
+    [SerializeField] private float sensY = 100f;
+    [SerializeField] private float returnSpeed = 6;
+
+    [SerializeField] Transform cam = null;
+    [SerializeField] Transform orientation = null;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        movement = GetComponent<PlayerMovement>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void LateUpdate()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+         
+        yRotation += mouseX * sensX * multiplier;
+        xRotation -= mouseY * sensY * multiplier;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, wallRun.tilt);
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
